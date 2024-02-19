9117f3e8ca0852f4d2e1e7a6b3fc852c84ac39fe
Adwait More <adwaitmore700@yahoo.com>
2024-02-19 14:06:28 -0800

    added changes to load images

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,1 +1,22 @@
 /* All Server Actions */
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: "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",
+  dangerouslyAllowBrowser: true,
+});
+
+export async function getImages(prompt) {
+  console.log(prompt);
+  try {
+    const response = await openai.images.generate({
+      model: "dall-e-3",
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+    return response.data;
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
--- app/lib/data.js
+++ app/lib/data.js
@@ -1,3 +1,14 @@
 /* All DB Queries */
 
-const { db } = require('@vercel/postgres');
+import { sql } from "@vercel/postgres";
+import { unstable_noStore as noStore } from "next/cache";
+
+export async function fetchSomething() {
+  noStore(); // to not cache stuffs
+
+  try {
+    //get data from sql
+  } catch (error) {
+    //throw error
+  }
+}
